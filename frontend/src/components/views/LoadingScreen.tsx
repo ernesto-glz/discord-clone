bda1b02d0d7a2520f5e9e07ad0abad271aeab572
kik0 <kik0swd@gmail.com>
2022-08-19 10:54:52 -0400

    Fixed loading screen compatibility with firefox

--- frontend/src/components/views/LoadingScreen.tsx
+++ frontend/src/components/views/LoadingScreen.tsx
@@ -1,6 +1,7 @@
 import React, { useMemo } from 'react';
 import { DiscordTips } from 'src/config/constants';
 import PageWrapper from 'src/pages/page-wrapper';
+import { Image } from './elements/Image';
 
 export const LoadingScreen: React.FC = () => {
   const randomTip = useMemo(() => Math.floor(Math.random() * DiscordTips.length), []);
@@ -8,9 +9,10 @@ export const LoadingScreen: React.FC = () => {
   return (
     <PageWrapper>
       <div className='loading-screen'>
-        <video className='spinner' autoPlay muted loop>
-          <source src={_r('/media/loading-spinner.webm')} />
-          <source src={_r('/media/loading-spinner.mp4')} type="video/mp4" />
+        <video autoPlay className='spinner' muted loop playsInline>
+          <source src={_r('/media/logo_spinner.webm')} type="video/webm" />
+          <source src={_r('/media/logo_spinner.mp4')} type="video/mp4" />
+          <Image src='/img/loading-skeleton.png' />
         </video>
         <h4 className='title'>Did you know</h4>
         <p className='tip'>{DiscordTips[randomTip]}</p>
